2e97c5846a60f0f2a8b22568a35952786c52acf3
David Keathley <david.keathley@q2ebanking.com>
2018-01-21 17:02:55 -0600

    Convert from react to cycle.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,26 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+// import xs from 'xstream'
+import {run} from '@cycle/run'
+import {makeDOMDriver} from '@cycle/dom'
+import 'snabbdom-jsx'
 
-const hello = () => "Hello World!";
 // main
-ReactDOM.render(
-    <h1>{hello()}</h1>,
-    document.getElementById('js-main')
-);
+function main(sources: any) {
+    const sinks = {
+        DOM: sources.DOM.select('input').events('click')
+            .map((ev: {target: {checked: boolean}}) => ev.target.checked)
+            .startWith(false)
+            .map((toggled: boolean) =>
+                <div>
+                    <input type="checkbox" /> Toggle Me
+                    <p>{toggled ? 'ON' : 'off'}</p>
+                </div>
+            )
+    }
+    return sinks
+}
+
+const drivers = {
+    DOM: makeDOMDriver('#app')
+};
+
+run(main, drivers)
